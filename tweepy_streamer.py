46a149431e72e2795d50a0c2d9081b2da684caa2
aunghoo <aung@umich.edu>
2020-08-12 21:17:42 -0400

    Tweet analyzer Simple Insights generation

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -86,6 +86,21 @@ class TwitterListener(StreamListener):
             return False
         print(status)
 
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweets.
+    """
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
 
 if __name__ == "__main__":
 
@@ -96,8 +111,16 @@ if __name__ == "__main__":
 
     tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
 
-    # print(dir(tweets[0]))
-    # print(tweets[0].retweet_count)
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
 
-    print(tweets)
+    # Get average length over all tweets.
+    print(np.mean(df['len']))
+
+    # Get the number of likes for the most liked tweet
+    print(np.max(df['likes']))
+
+    # Get the number of retweets for the most retweeted tweet
+    print(np.max(df['retweets']))
+
+    # print(df.head(10))
     
